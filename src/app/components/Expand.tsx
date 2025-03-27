9f78c4c4273e7ac50a58cc24b8b3c20a43b0a0ce
toobatux <toobatux@gmail.com>
2025-03-27 02:03:09 -0400

    arrow animation

--- src/app/components/Expand.tsx
+++ src/app/components/Expand.tsx
@@ -9,10 +9,28 @@ import Link from "next/link";
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Expand = () => {
+  const section = useRef(null);
   const arrow = useRef(null);
 
   useGSAP(() => {
     const tl = gsap.timeline();
+
+    gsap.set(section.current, {
+      y: 0,
+      opacity: 1,
+    });
+
+    gsap.to(section.current, {
+      y: 0,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: section.current,
+        start: "top 40%",
+        end: "top 2%",
+        scrub: true,
+        toggleActions: "play reverse play reverse",
+      },
+    });
 
     gsap.set(arrow.current, {
       opacity: 0,
@@ -43,11 +61,12 @@ const Expand = () => {
   });
   return (
     <div
-      ref={arrow}
       className="absolute bottom-14 right-0 left-0 flex justify-center"
+      ref={section}
     >
       <Link
         href="#info"
+        ref={arrow}
         className="p-2 rounded-full hover:bg-white/10 transition-colors"
       >
         <ExpandMore className="text-white/55" />
