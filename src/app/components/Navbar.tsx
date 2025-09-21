146e21154bcbc42c3364137045973fc8400e0ba9
toobatux <toobatux@gmail.com>
2025-09-21 00:40:26 -0400

    nav update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -40,24 +40,51 @@ const Navbar = () => {
       <div className="flex w-full">
         <div className="flex-auto max-w-7xl mx-auto">
           <div className="">
-            <div className="relative flex w-full justify-between px-8 lg:px-12 py-6 transition-all">
+            <div className="relative flex w-full justify-between px-8 lg:px-12 h-[72px] transition-all">
               <Link href="/" className="flex items-center">
                 <div className="text-white">T/K</div>
               </Link>
-              <div className="flex gap-4 items-center">
+              <div className="flex items-center gap-5">
                 {navLinks.map((link) => {
                   return (
-                    <Link
-                      href={link.href}
+                    // <Link
+                    //   href={link.href}
+                    //   key={link.name}
+                    //   className={
+                    //     link.active
+                    //       ? "text-white underline ps-2"
+                    //       : "text-white/65 hover:underline hover:text-white no-underline ps-2 transition-colors"
+                    //   }
+                    // >
+                    //   {link.name}
+                    // </Link>
+
+                    <div
+                      className="relative h-full items-center group"
                       key={link.name}
-                      className={
-                        link.active
-                          ? "text-white underline ps-2"
-                          : "text-white/65 hover:underline hover:text-white no-underline ps-2 transition-colors"
-                      }
                     >
-                      {link.name}
-                    </Link>
+                      <Link
+                        href={link.href}
+                        className={`${
+                          link.active
+                            ? "text-foreground"
+                            : "text-white/60 hover:text-white"
+                        } h-full flex items-center duration-200`}
+                      >
+                        {link.name}
+                      </Link>
+                      {/* {link.active && (
+              <div className="absolute bottom-0 w-full h-[2px] bg-foreground"></div>
+            )} */}
+
+                      <div
+                        className={`absolute bottom-0 w-full transition-all duration-200 ${
+                          link.active
+                            ? "h-[2px] bg-primary"
+                            : "h-[0px] group-hover:h-[2px] bg-white/50"
+                        }`}
+                      ></div>
+                    </div>
                   );
                 })}
               </div>
